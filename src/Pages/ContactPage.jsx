d46a16ee9dc038e539092d2de6c4c36635421f4f
aitezazdev <aitezazsikandar@gmail.com>
2025-01-29 23:03:20 +0500

    fix contact page

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { FaArrowDown } from 'react-icons/fa';
 
-const ContactPage = () => {a
+const ContactPage = () => {
   return (
-    <div className="flex px-28 pt-32 pb-10 flex-col items-center justify-center text-center p-6">
+    <div className="flex px-28 pt-32 pb- flex-col items-center justify-center text-center p-6">
       <h1 className="text-4xl font-bold text-red-500 mb-4">Get in Touch</h1>
       <p className="text-gray-600 max-w-xl mb-6">
         Have questions or need assistance? Check out our contact details in the footer below.
